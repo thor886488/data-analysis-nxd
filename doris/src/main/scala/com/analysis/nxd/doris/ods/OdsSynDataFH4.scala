04ff3de841628422efdaa57d8a2aeac2db388cde
thor <18818406784@163.com>
2024-08-16 23:39:56 +0800

    fix

--- doris/src/main/scala/com/analysis/nxd/doris/ods/OdsSynDataFH4.scala
+++ doris/src/main/scala/com/analysis/nxd/doris/ods/OdsSynDataFH4.scala
@@ -73,7 +73,7 @@ object OdsSynDataFH4 {
     val startUpdateTime = DateUtils.tranDorisToOracleTimestamp(startUpdateTimeP)
     val endTime = DateUtils.tranDorisToOracleTimestamp(endTimeP).replace(".000000", ".999999")
     logger.warn(s" startTime : '$startTime'  , endTime '$endTime', isDeleteData '$isDeleteData'")
-    val sql_ods_fh4_user_customer =
+    val sql_ods_fh4_user_customer_2 =
       s"""
          |INSERT INTO ods_fh4_user_customer_2
          |select register_date,'FH4' site_code,id,account,passwd,passwd_lvl,withdraw_passwd,cipher,sex,email,email_actived,cellphone,birthday,qq_structure,is_freeze,user_lvl,qu_struc,withdraw_passwd_active_date,question_structure_active_date,register_ip,parent_id,user_chain,last_login_date,term_a_count,freeze_date,freezer,vip_cellphone,term_u_account,agent_limit,freeze_method,last_login_ip,freeze_memo,freeze_account,unfreeze_date,freeze_id,vip_lvl,referer,url_id,bind_date,bind_phone_serial,unbind_type,phone_serial_num,phone_type,source,device,award_ret_status,super_pair_status,modify_passwd_date,appeal_new_func,nick_name,head_img,nick_update_time,lhc_status,wechat,pk10_status,max_award,auto_trans_flag,joint_venture,ga_id,new_vip_flag
@@ -111,11 +111,9 @@ object OdsSynDataFH4 {
         |""".stripMargin
 
 
+    JdbcUtils.execute(conn, "sql_ods_fh4_user_customer_2", sql_ods_fh4_user_customer_2)
     JdbcUtils.execute(conn, "sql_ods_fh4_user_customer", sql_ods_fh4_user_customer)
     JdbcUtils.execute(conn, "sql_ods_fh4_user_chain_backup", sql_ods_fh4_user_chain_backup)
-
-    JdbcUtils.execute(conn, "sql_ods_fh4_user_customer", sql_ods_fh4_user_customer)
-
     JdbcUtils.execute(conn, "sql_ods_fh4_fund", sql_ods_fh4_fund)
 
     val ids: String = JdbcUtils.queryListStr(null, conn, s"select user_id from   ods_fh4_user_chain_backup where  (create_date>='$startUpdateTimeP' and  create_date<='$endTimeP') limit 999 " , "")
@@ -124,14 +122,27 @@ object OdsSynDataFH4 {
       System.out.print(ids2)
       val sql_ods_fh4_user_customer_2 =
         s"""
-           |INSERT INTO ods_fh4_user_customer
+           |INSERT INTO ods_fh4_user_customer_2
            |select register_date,'FH4' site_code,id,account,passwd,passwd_lvl,withdraw_passwd,cipher,sex,email,email_actived,cellphone,birthday,qq_structure,is_freeze,user_lvl,qu_struc,withdraw_passwd_active_date,question_structure_active_date,register_ip,parent_id,user_chain,last_login_date,term_a_count,freeze_date,freezer,vip_cellphone,term_u_account,agent_limit,freeze_method,last_login_ip,freeze_memo,freeze_account,unfreeze_date,freeze_id,vip_lvl,referer,url_id,bind_date,bind_phone_serial,unbind_type,phone_serial_num,phone_type,source,device,award_ret_status,super_pair_status,modify_passwd_date,appeal_new_func,nick_name,head_img,nick_update_time,lhc_status,wechat,pk10_status,max_award,auto_trans_flag,joint_venture,ga_id,new_vip_flag
            |from   syn_oracle_fh4_user_customer
            |where  id in ($ids2)
-           |and   account not like  'guest%'
+           |and   account not like  'guest%'  and  passwd not  LIKE  '%�%'
            |""".stripMargin
+
+      val sql_ods_fh4_user_customer =
+        s"""
+           |INSERT INTO ods_fh4_user_customer
+           |select register_date,'FH4' site_code,id,account,passwd,passwd_lvl,withdraw_passwd,cipher,sex,email,email_actived,cellphone,birthday,qq_structure,is_freeze,user_lvl,qu_struc,withdraw_passwd_active_date,question_structure_active_date,register_ip,parent_id,user_chain,last_login_date,term_a_count,freeze_date,freezer,vip_cellphone,term_u_account,agent_limit,freeze_method,last_login_ip,freeze_memo,freeze_account,unfreeze_date,freeze_id,vip_lvl,referer,url_id,bind_date,bind_phone_serial,unbind_type,phone_serial_num,phone_type,source,device,award_ret_status,super_pair_status,modify_passwd_date,appeal_new_func,nick_name,head_img,nick_update_time,lhc_status,wechat,pk10_status,max_award,auto_trans_flag,joint_venture,ga_id,new_vip_flag
+           |from   ods_fh4_user_customer_2
+           |where  id in ($ids2)
+           |and   account not like  'guest%'  and  passwd not  LIKE  '%�%'
+           |""".stripMargin
+
       JdbcUtils.execute(conn, "sql_ods_fh4_user_customer_2", sql_ods_fh4_user_customer_2)
+      JdbcUtils.execute(conn, "sql_ods_fh4_user_customer", sql_ods_fh4_user_customer)
+
     }
+
 
 
   }
